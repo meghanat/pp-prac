b53ec274d311d9ab7b4892d487a763ffea27110a
meghanat <meghs.t@gmail.com>
2015-02-05 07:13:24 +0530

    convert __call__ into smaller funcs

--- simulator/LRUAlgorithm.py
+++ simulator/LRUAlgorithm.py
@@ -24,23 +24,67 @@ class LRU(object):
     def stop_lru(self):
         self.simulating = False
 
+    #fill empty frame
+    def fill_frame(self,virtual_page_no,pid,frame_no):
+        page_table=self.page_tables[pid]
+        #  Update the page table of this process
+        if virtual_page_no not in page_table:
+            page_table[virtual_page_no] = {}
+        page_table[virtual_page_no]["frame_no"] = frame_no
+        page_table[virtual_page_no]["present_bit"] = 1
+
+        #  Update the frame entry also
+        self.memory[frame_no]["pid"] = pid
+        self.memory[frame_no]["time"] = time.clock()
+        self.memory[frame_no]["virtual_page_no"] = virtual_page_no
+
+
+    #swap out page from memory
+    def replace_frame(self,virtual_page_no,pid):
+        #print "here"
+        frame_to_replace = min(self.memory, key=lambda x: x["time"])
+        frame_no_to_replace = self.memory.index(frame_to_replace)
+
+        # For the frame which is being replaced, make it's process'
+        # page table's page table entry's present bit 1
+        self.page_tables[frame_to_replace["pid"]]\
+        [frame_to_replace["virtual_page_no"]]["present_bit"] = 0
+
+        # For the current process' page table
+        page_table=self.page_tables[pid]
+        if virtual_page_no not in page_table:
+            page_table[virtual_page_no] = {}
+
+        page_table[virtual_page_no]["frame_no"] = frame_no_to_replace
+        page_table[virtual_page_no]["present_bit"] = 1
+
+        # Update the frame entry's PID and time stamp
+        self.memory[frame_no_to_replace]["pid"] = pid
+        self.memory[frame_no_to_replace]["time"] = time.clock()
+        self.memory[frame_no_to_replace]["virtual_page_no"] = virtual_page_no
+
+    #return page table for process    
+    def get_page_table(self,pid):
+        if pid not in self.page_tables:
+            self.page_tables[pid] = {}
+        page_table = self.page_tables[pid]
+        return page_table
         
     def __call__(self):
         while self.simulating:
             if(len(self.page_num_stream) == 0):  # Wait for an access to be made
-                self.event.clear()
+                self.event.clear() 
             self.event.wait()
 
-            print self.page_num_stream[0]
+            #print self.page_num_stream[0]
             pid, virtual_page_no, thread_set = self.page_num_stream[0]
             thread_id = thread.get_ident()
 
             if thread_id not in thread_set:  # Only if the thread hasn't already
                                              # read this address
-                if pid not in self.page_tables:
-                    self.page_tables[pid] = {}
-                
-                page_table = self.page_tables[pid]
+                #get page table for process
+                page_table=self.get_page_table(pid)
+
                 if virtual_page_no in page_table:
                     pte = page_table[virtual_page_no]
                 else:
@@ -49,45 +93,16 @@ class LRU(object):
                 if pte and pte["present_bit"]:
                     #  Update main memory time stamp
                     self.memory[pte["frame_no"]]["time"] = time.clock()
-                else:
+                
+                else:   #page not in memory
                     for frame_no, frame_entry in enumerate(self.memory):
                         if frame_entry["pid"] == -1:  # Empty Frame
-                            #  Update the page table of this process
-                            if virtual_page_no not in page_table:
-                                page_table[virtual_page_no] = {}
-                            page_table[virtual_page_no]["frame_no"] = frame_no
-                            page_table[virtual_page_no]["present_bit"] = 1
-
-                            #  Update the frame entry also
-                            self.memory[frame_no]["pid"] = pid
-                            self.memory[frame_no]["time"] = time.clock()
-                            self.memory[frame_no]["virtual_page_no"] = virtual_page_no
-
-                            #print page_table
-                            #print self.memory[frame_no]
+                            self.fill_frame(virtual_page_no,pid,frame_no)
                             break
                     else:  # If all of the previous iterations went through,
                            # ie. if no frames are free
-                        frame_to_replace = min(self.memory, key=lambda x: x["time"])
-                        frame_no_to_replace = self.memory.index(frame_to_replace)
-
-                        # For the frame which is being replaced, make it's process'
-                        # page table's page table entry's present bit 1
-                        self.page_tables[frame_to_replace["pid"]]\
-                        [frame_to_replace["virtual_page_no"]]["present_bit"] = 0
-
-                        # For the current process' page table
-                        if virtual_page_no not in page_table:
-                            page_table[virtual_page_no] = {}
-
-                        page_table[virtual_page_no]["frame_no"] = frame_no_to_replace
-                        page_table[virtual_page_no]["present_bit"] = 1
-
-                        # Update the frame entry's PID and time stamp
-                        self.memory[frame_no_to_replace]["pid"] = pid
-                        self.memory[frame_no_to_replace]["time"] = time.clock()
-                        self.memory[frame_no_to_replace]["virtual_page_no"] = virtual_page_no
-
+                        self.replace_frame(virtual_page_no,pid)
+                        
                 self.page_num_stream[0][2].add(thread_id)  # This thread has read the address
             
             if(len(self.page_num_stream[0][2]) == self.number_pr_threads):  # If all threads have read the value
