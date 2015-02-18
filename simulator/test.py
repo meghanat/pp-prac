c453b9df3b5083b79e07396c42de73c61f24244b
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-02-18 21:51:08 +0530

    Fix var names

--- simulator/test.py
+++ simulator/test.py
@@ -31,16 +31,16 @@ def test_optimal_page_fault_count():
     thread_set = set()
 
     page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    lruob = opt.Optimal(100, number_frames, number_pr_threads, 
+    opt_ob = opt.Optimal(100, number_frames, number_pr_threads, 
                        page_num_stream, event_page_stream, read_lock, thread_set)
-    thread_lru = threading.Thread(target=lruob, args=(None,))
-    thread_lru.start()
+    thread_opt = threading.Thread(target=opt_ob, args=(None,))
+    thread_opt.start()
     while len(page_num_stream) > 0:
         event_page_stream.set()
         pass
-    thread_lru._Thread__stop()
-    print lruob.get_page_fault_count()
-    assert expected_faults == lruob.get_page_fault_count()
+    thread_opt._Thread__stop()
+    print opt_ob.get_page_fault_count()
+    assert expected_faults == opt_ob.get_page_fault_count()
 
 test_optimal_page_fault_count()
 
