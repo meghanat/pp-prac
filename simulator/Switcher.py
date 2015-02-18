11ea9f5c5ce26c84424e7557146dfd7da5d81a26
meghst <meghana.thiyyakat@flipkart.com>
2015-02-18 20:37:39 +0530

    Pass Optimal to switcher's init
    Since Optimal also reads from the same address stream, and it needs to show the page faults for the same set of pages as the other algorithms, it is necessary to perform the same sleep operation on Optimal as well after the defined number of page accesses.
    
    But since Optimal is not a candidate for switching, it is not passed as a part of the other_algorithm[] list. Hence, the resetting of its local page fault count and number of pages accessed is done outside the loop operating on other_algorithm[]

--- simulator/Switcher.py
+++ simulator/Switcher.py
@@ -1,22 +1,25 @@
 import time
 class Switcher(object):
-	def __init__(self,current_algorithm,other_algorithms):
+	def __init__(self,current_algorithm,other_algorithms,optimal):
 		self.current_algorithm=current_algorithm
 		self.other_algorithms=other_algorithms
+		self.optimal=optimal
 
 	def switch(self):
-		print "SWITChER"
+		print "SWITCHER"
 		time.sleep(5)
 		for i in self.other_algorithms:
 			i.reset_memory(self.current_algorithm.memory)
 			i.page_tables=dict(self.current_algorithm.page_tables)
 			i.page_fault_count=0
+		self.optimal.page_fault_count=0 # since optimal is also passed but isnt part of other_algorithms[]
 		
 		self.current_algorithm=min(self.other_algorithms,key=lambda x: x.get_page_fault_count)
 		print self.current_algorithm.name
 
 		for i in self.other_algorithms:
 			i.pages_accessed=0
+		self.optimal.pages_accessed=0 # since the pages being accessed must be the same across all algorithms	
 
 
 
