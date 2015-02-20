31ffd8428a31c6e78d33168136c36b9be4b46a28
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-02-20 19:47:01 +0530

    Fix switch of Switcher

--- simulator/Switcher.py
+++ simulator/Switcher.py
@@ -6,21 +6,20 @@ class Switcher(object):
 		self.optimal=optimal
 
 	def switch(self):
-		print "SWITCHER"
-		time.sleep(5)
+		print "Switch"
+		best = min(self.other_algorithms, key=lambda x: x.get_page_fault_count())
+		print self.best.name
+
 		for i in self.other_algorithms:
 			i.reset_memory(self.current_algorithm.memory)
 			i.page_tables=dict(self.current_algorithm.page_tables)
 			i.page_fault_count=0
 		self.optimal.page_fault_count=0 # since optimal is also passed but isnt part of other_algorithms[]
-		
-		self.current_algorithm=min(self.other_algorithms,key=lambda x: x.get_page_fault_count)
-		print self.current_algorithm.name
-
+	
 		for i in self.other_algorithms:
 			i.pages_accessed=0
 		self.optimal.pages_accessed=0 # since the pages being accessed must be the same across all algorithms	
 
-
+		self.current_algorithm = best
 
 
