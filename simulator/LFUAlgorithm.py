81340eeb25f49a377af31ef587e5a6c8ee119f6e
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-02-20 19:59:32 +0530

    Make switching_window=simulation_window in LFU

--- simulator/LFUAlgorithm.py
+++ simulator/LFUAlgorithm.py
@@ -3,7 +3,7 @@ import thread
 
 class LFU(object):
     def __init__(self, number_virtual_pages, number_frames, number_pr_threads, 
-                       page_num_stream, event_page_stream, read_lock, thread_set,simulation_window_size=10, switching_window=10000):
+                       page_num_stream, event_page_stream, read_lock, thread_set,simulation_window_size=10):
         self.number_virtual_pages = number_virtual_pages
         self.number_pr_threads = number_pr_threads
         self.page_tables = { }  # Structure: PID => Page Table
@@ -19,7 +19,6 @@ class LFU(object):
         self.thread_set = thread_set
         self.simulation_window_size = simulation_window_size
         self.pages_accessed = 0
-        self.switching_window = switching_window
         self.name = "LFU"
 
 
@@ -40,7 +39,7 @@ class LFU(object):
 
     #fill empty frame
     def fill_frame(self,virtual_page_no,pid,frame_no):
-        print "LFU replace"
+        print "LFU: Fill frame ", frame_no, " with ", virtual_page_no, "\n"
         page_table=self.page_tables[pid]
         #  Update the page table of this process
         if virtual_page_no not in page_table:
@@ -74,6 +73,9 @@ class LFU(object):
         page_table[virtual_page_no]["frame_no"] = frame_no_to_replace
         page_table[virtual_page_no]["present_bit"] = 1
 
+        print "LFU: Replace page ", self.memory[frame_no_to_replace]["virtual_page_no"],\
+              " in frame ", frame_no_to_replace, " with page ", virtual_page_no, " of process ", pid, "\n"
+
         # Update the frame entry's PID and frequency
         self.memory[frame_no_to_replace]["pid"] = pid
         self.memory[frame_no_to_replace]["frequency"] = 0
@@ -90,7 +92,7 @@ class LFU(object):
     def __call__(self,switcher):
         self.switcher=switcher
         while True:
-            while(self.pages_accessed == self.switching_window):
+            while(self.pages_accessed == self.simulation_window_size):
                 pass
             thread_id = thread.get_ident()
 
@@ -140,7 +142,7 @@ class LFU(object):
                 if(len(self.page_num_stream) < self.simulation_window_size):  # Wait for an access to be made
                     self.event.clear() 
 
-                if(self.pages_accessed == self.switching_window):
+                if(self.pages_accessed == self.simulation_window_size):
                     self.switcher.switch()
                 
             self.read_lock.release()
