ecf5c75b646b1b06b61188b3c6a5105f2dd79af8
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-02-25 20:28:15 +0530

    Move __call__ to superclass Algorithm

--- simulator/Algorithm.py
+++ simulator/Algorithm.py
@@ -47,3 +47,63 @@ class Algorithm:
     def stop_algorithm(self):
         self.simulating = False
 
+    # This method *must* be overidden in the subclass algorithms
+    def update_frame_in_memory(self, pte):
+        pass
+
+    def __call__(self,switcher):
+        self.switcher=switcher
+        while self.simulating:
+
+            while(self.pages_accessed == self.simulation_window_size):
+                pass
+            
+            thread_id = thread.get_ident()
+
+            if thread_id not in self.thread_set:  # Only if the thread hasn't already
+                                             # read this address
+
+                self.read_lock.acquire()
+                self.event.wait()
+                
+                pid, virtual_page_no = self.page_num_stream[0]
+
+                self.read_lock.release()
+                self.pages_accessed+=1
+
+                #get page table for process
+                page_table=self.get_page_table(pid)
+
+                if virtual_page_no in page_table:
+                    pte = page_table[virtual_page_no]
+                else:
+                    pte = None
+
+                if pte and pte["present_bit"]:
+                    #  Update main memory time stamp
+                    self.update_frame_in_memory(pte)
+                
+                else:   #page not in memory
+                    for frame_no, frame_entry in enumerate(self.memory):
+                        if frame_entry["pid"] == -1:  # Empty Frame
+                            self.fill_frame(virtual_page_no,pid,frame_no)
+                            break
+                    else:  # If all of the previous iterations went through,
+                           # ie. if no frames are free
+                        self.replace_frame(virtual_page_no,pid)
+
+                self.thread_set.add(thread_id)  # This thread has read the address
+            self.read_lock.acquire()
+            #print thread_id, " LRU in 1"
+            if(len(self.page_num_stream) != 0 and len(self.thread_set) == self.number_pr_threads):  # If all threads have read the value
+                #print "Popped"
+                self.page_num_stream.pop(0)
+                self.thread_set.clear()
+                if(len(self.page_num_stream) < self.simulation_window_size):  # Wait for an access to be made
+                    self.event.clear() 
+
+                if(self.pages_accessed == self.simulation_window_size):
+                    self.switcher.switch()
+                
+            self.read_lock.release()
+            #print thread_id, " LRU out 1"
