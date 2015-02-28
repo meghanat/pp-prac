e9bae2e75529b38c3f4f4c01844c72e5f2425b7c
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-02-28 22:58:56 +0530

    Make tests work with the new interface

--- simulator/test.py
+++ simulator/test.py
@@ -9,14 +9,16 @@ def test_lru_page_fault_count():
     number_frames = 3
     number_pr_threads = 1
     event_page_stream = threading.Event()
+    switching_event = threading.Event()
+    switching_event.set()
     event_page_stream.set()
     read_lock = threading.Lock()
     expected_faults = 6
     thread_set = set()
-
+    simulation_window_size = 10
     page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    lruob = lru.LRU(100, number_frames, number_pr_threads, 
-                       page_num_stream, event_page_stream, read_lock, thread_set)
+    lruob = lru.LRU(number_frames, number_pr_threads, page_num_stream, event_page_stream, 
+                    read_lock, thread_set, simulation_window_size, switching_event)
     thread_lru = threading.Thread(target=lruob, args=(None,))
     thread_lru.start()
     while len(page_num_stream) > 0:
@@ -30,13 +32,16 @@ def test_optimal_page_fault_count():
     number_pr_threads = 1
     event_page_stream = threading.Event()
     event_page_stream.set()
+    switching_event = threading.Event()
+    switching_event.set()
     read_lock = threading.Lock()
     expected_faults = 5
     thread_set = set()
+    simulation_window_size = 10
 
     page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    opt_ob = opt.Optimal(100, number_frames, number_pr_threads, 
-                       page_num_stream, event_page_stream, read_lock, thread_set)
+    opt_ob = opt.Optimal(number_frames, number_pr_threads, page_num_stream, event_page_stream,
+                         read_lock, thread_set, simulation_window_size, switching_event)
     thread_opt = threading.Thread(target=opt_ob, args=(None,))
     thread_opt.start()
     while len(page_num_stream) > 0:
@@ -51,13 +56,16 @@ def test_fifo_page_fault_count():
     number_pr_threads = 1
     event_page_stream = threading.Event()
     event_page_stream.set()
+    switching_event = threading.Event()
+    switching_event.set()
     read_lock = threading.Lock()
     expected_faults = 7
     thread_set = set()
+    simulation_window_size = 10
 
     page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    fifo_ob = fifo.FIFO(100, number_frames, number_pr_threads, 
-                       page_num_stream, event_page_stream, read_lock, thread_set)
+    fifo_ob = fifo.FIFO(number_frames, number_pr_threads, page_num_stream, event_page_stream,
+                        read_lock, thread_set, simulation_window_size, switching_event)
 
     thread_fifo = threading.Thread(target=fifo_ob, args=(None,))
     thread_fifo.start()
@@ -73,13 +81,16 @@ def test_lfu_page_fault_count():
     number_pr_threads = 1
     event_page_stream = threading.Event()
     event_page_stream.set()
+    switching_event = threading.Event()
+    switching_event.set()
     read_lock = threading.Lock()
     expected_faults = 7
     thread_set = set()
+    simulation_window_size = 10
 
     page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    lfuob = lfu.LFU(100, number_frames, number_pr_threads, 
-                       page_num_stream, event_page_stream, read_lock, thread_set)
+    lfuob = lfu.LFU(number_frames, number_pr_threads, page_num_stream, event_page_stream,
+                    read_lock, thread_set, simulation_window_size, switching_event)
     thread_lfu = threading.Thread(target=lfuob, args=(None,))
     thread_lfu.start()
     while len(page_num_stream) > 0:
