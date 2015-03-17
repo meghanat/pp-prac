1fd87caa7994f09c43ef16539ebc18e634070a8f
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-03-17 22:02:27 +0530

    Refactor update_labels of sim

--- simulator/Application.py
+++ simulator/Application.py
@@ -232,16 +232,14 @@ class Simulator(tk.Tk):
     def update_labels(self):
         while self.simulation_values["simulating"]:
             self.total_count.set(self.controller.switcher.get_total_count())
-            self.algo_values["LRU"]["string_var"].set(str(self.controller.lru.get_page_fault_count()))
-            self.algo_values["LFU"]["string_var"].set(str(self.controller.lfu.get_page_fault_count()))
-            self.algo_values["OPTIMAL"]["string_var"].set(str(self.controller.optimal.get_page_fault_count()))
-            self.algo_values["FIFO"]["string_var"].set(str(self.controller.fifo.get_page_fault_count()))
-            self.algo_values["RANDOM"]["string_var"].set(str(self.controller.random.get_page_fault_count()))
-            self.algo_values["CLOCK"]["string_var"].set(str(self.controller.clock.get_page_fault_count()))
 
+            for algo in self.algo_values:
+                self.algo_values[algo]["string_var"].set(str(self.algo_values[algo]["algo"].get_page_fault_count()))
+            
             for algo in self.algo_values:
                 self.algo_values[algo]["label"].config(bg="white")
             self.algo_values[self.controller.switcher.current_algorithm.name]["label"].config(bg="green")
+            
             time.sleep(1)
             self.update_idletasks()
 
