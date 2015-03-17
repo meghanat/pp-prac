82269a2562269cafd06b0480eed7d4170f119fe0
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-03-17 20:23:29 +0530

    Update tests to conform to new interface and use unittest

--- simulator/test.py
+++ simulator/test.py
@@ -3,105 +3,74 @@ import LRUAlgorithm as lru
 import FIFOAlgorithm as fifo
 import LFUAlgorithm as lfu
 import threading
+import unittest
 
-# Test the page fault count logic
-def test_lru_page_fault_count():
-    number_frames = 3
-    number_pr_threads = 1
-    event_page_stream = threading.Event()
-    switching_event = threading.Event()
-    switching_event.set()
-    event_page_stream.set()
-    read_lock = threading.Lock()
-    expected_faults = 6
-    thread_set = set()
-    simulation_window_size = 10
-    page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    lruob = lru.LRU(number_frames, number_pr_threads, page_num_stream, event_page_stream, 
-                    read_lock, thread_set, simulation_window_size, switching_event)
-    thread_lru = threading.Thread(target=lruob, args=(None,))
-    thread_lru.start()
-    while len(page_num_stream) > 0:
-        event_page_stream.set()
-        pass
-    thread_lru._Thread__stop()
-    assert expected_faults == lruob.get_page_fault_count()
 
-def test_optimal_page_fault_count():
-    number_frames = 3
-    number_pr_threads = 1
-    event_page_stream = threading.Event()
-    event_page_stream.set()
-    switching_event = threading.Event()
-    switching_event.set()
-    read_lock = threading.Lock()
-    expected_faults = 5
-    thread_set = set()
-    simulation_window_size = 10
+class TestAlgorithmPageFaultCounts(unittest.TestCase):
 
-    page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    opt_ob = opt.Optimal(number_frames, number_pr_threads, page_num_stream, event_page_stream,
-                         read_lock, thread_set, simulation_window_size, switching_event)
-    thread_opt = threading.Thread(target=opt_ob, args=(None,))
-    thread_opt.start()
-    while len(page_num_stream) > 0:
-        event_page_stream.set()
-        pass
-    thread_opt._Thread__stop()
-    print opt_ob.get_page_fault_count()
-    assert expected_faults == opt_ob.get_page_fault_count()
+    def setUp(self):
+        self.simulation_values = {}
+        self.simulation_values["number_frames"] = 3
+        self.simulation_values["vas"] = 4
+        self.simulation_values["number_processes"] = 1
 
-def test_fifo_page_fault_count():
-    number_frames = 3
-    number_pr_threads = 1
-    event_page_stream = threading.Event()
-    event_page_stream.set()
-    switching_event = threading.Event()
-    switching_event.set()
-    read_lock = threading.Lock()
-    expected_faults = 7
-    thread_set = set()
-    simulation_window_size = 10
+        self.simulation_values["page_num_stream"] = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
+        self.simulation_values["event_page_stream"] = threading.Event()  
+        self.simulation_values["event_page_stream"].set()
+        self.simulation_values["switching_event"] = threading.Event()
+        self.simulation_values["switching_event"].set()
+        self.simulation_values["read_lock"] = threading.Lock()                                 
+        self.simulation_values["process_size"] = self.simulation_values["vas"] * (2 ** 30)  # GB 
+        self.simulation_values["number_pr_threads"] = 1 # No of page replacement algorithms
+        self.simulation_values["thread_set"] = set()  # Global set; Used to indicate reading of an elem by all algos.
+        self.simulation_values["window"] = 10  
+        self.simulation_values["simulating"] = True      
+        self.simulation_values["read_from_file"] = False  
 
-    page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    fifo_ob = fifo.FIFO(number_frames, number_pr_threads, page_num_stream, event_page_stream,
-                        read_lock, thread_set, simulation_window_size, switching_event)
+    def test_lru_page_fault_count(self):
+        expected_faults = 6
+        lruob = lru.LRU(self.simulation_values)
+        thread_lru = threading.Thread(target=lruob, args=(None,))
+        thread_lru.start()
+        while len(self.simulation_values["page_num_stream"]) > 0:
+            self.simulation_values["event_page_stream"].set()
+            self.simulation_values["switching_event"].set()
 
-    thread_fifo = threading.Thread(target=fifo_ob, args=(None,))
-    thread_fifo.start()
-    while len(page_num_stream) > 0:
-        event_page_stream.set()
-        pass
-    thread_fifo._Thread__stop()
-    print fifo_ob.get_page_fault_count()
-    assert expected_faults == fifo_ob.get_page_fault_count()
+        thread_lru._Thread__stop()
+        self.assertEqual(expected_faults, lruob.get_page_fault_count())
 
-def test_lfu_page_fault_count():
-    number_frames = 3
-    number_pr_threads = 1
-    event_page_stream = threading.Event()
-    event_page_stream.set()
-    switching_event = threading.Event()
-    switching_event.set()
-    read_lock = threading.Lock()
-    expected_faults = 7
-    thread_set = set()
-    simulation_window_size = 10
+    def test_optimal_page_fault_count(self):
+        expected_faults = 5
+        opt_ob = opt.Optimal(self.simulation_values)
+        thread_opt = threading.Thread(target=opt_ob, args=(None,))
+        thread_opt.start()
+        while len(self.simulation_values["page_num_stream"]) > 0:
+            self.simulation_values["event_page_stream"].set()
+            self.simulation_values["switching_event"].set()
+        thread_opt._Thread__stop()
+        self.assertEqual(expected_faults, opt_ob.get_page_fault_count())
 
-    page_num_stream = [[1, 2], [1, 3], [1, 2], [1, 1], [1, 5], [1, 2], [1, 4], [1, 5], [1, 3]]
-    lfuob = lfu.LFU(number_frames, number_pr_threads, page_num_stream, event_page_stream,
-                    read_lock, thread_set, simulation_window_size, switching_event)
-    thread_lfu = threading.Thread(target=lfuob, args=(None,))
-    thread_lfu.start()
-    while len(page_num_stream) > 0:
-        event_page_stream.set()
-        pass
-    thread_lfu._Thread__stop()
-    print lfuob.get_page_fault_count()
-    assert expected_faults == lfuob.get_page_fault_count()
+    def test_fifo_page_fault_count(self):
+        expected_faults = 7
+        fifo_ob = fifo.FIFO(self.simulation_values)
+        thread_fifo = threading.Thread(target=fifo_ob, args=(None,))
+        thread_fifo.start()
+        while len(self.simulation_values["page_num_stream"]) > 0:
+            self.simulation_values["event_page_stream"].set()
+            self.simulation_values["switching_event"].set()
+        thread_fifo._Thread__stop()
+        self.assertEqual(expected_faults, fifo_ob.get_page_fault_count())        
 
-test_lru_page_fault_count()
-test_optimal_page_fault_count()
-test_fifo_page_fault_count()
-test_lfu_page_fault_count()
+    def test_lfu_page_fault_count(self):
+        expected_faults = 7
+        lfuob = lfu.LFU(self.simulation_values)
+        thread_lfu = threading.Thread(target=lfuob, args=(None,))
+        thread_lfu.start()
+        while len(self.simulation_values["page_num_stream"]) > 0:
+            self.simulation_values["event_page_stream"].set()
+            self.simulation_values["switching_event"].set()
+        thread_lfu._Thread__stop()
+        self.assertEqual(expected_faults, lfuob.get_page_fault_count())        
 
+if __name__ == '__main__':
+    unittest.main()
