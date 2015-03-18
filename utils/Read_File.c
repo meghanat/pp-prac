22eea86e73c464aa15d227ec5e614404d43133ad
Aishwarya <kallimani.aishwarya@gmail.com>
2015-03-18 19:34:14 +0530

    Reading file from kernel module added

---
+++ utils/Read_File.c
@@ -0,0 +1,42 @@
+#include <linux/module.h>  // Needed by all modules
+#include <linux/kernel.h>  // Needed for KERN_INFO
+#include <linux/fs.h>      // Needed by filp
+#include <asm/uaccess.h>   // Needed by segment descriptors
+
+int init_module(void)
+{
+    // Create variables
+    struct file *f;
+    char buf[128];
+    mm_segment_t fs;
+    int i;
+    // Init the buffer with 0
+    for(i=0;i<128;i++)
+        buf[i] = 0;
+    // To see in /var/log/messages that the module is operating
+    printk(KERN_INFO "My module is loaded\n");
+    // I am using Fedora and for the test I have chosen following file
+    // Obviously it is much smaller than the 128 bytes, but hell with it =)
+    f = filp_open("/home/aishwarya/My_blog2", O_RDONLY, 0);
+    if(f == NULL)
+        printk(KERN_ALERT "filp_open error!!.\n");
+    else{
+        // Get current segment descriptor
+        fs = get_fs();
+        // Set segment descriptor associated to kernel space
+        set_fs(get_ds());
+        // Read the file
+        f->f_op->read(f, buf, 128, &f->f_pos);
+        // Restore segment descriptor
+        set_fs(fs);
+        // See what we read from file
+        printk(KERN_INFO "buf:%s\n",buf);
+    }
+    filp_close(f,NULL);
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    printk(KERN_INFO "My module is unloaded\n");
+}
