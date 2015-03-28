c3c574a3158e29b28cd0fbe586764e8b4313e1ea
Deborah Gertrude Digges <deborah.gertrude.digges@gmail.com>
2015-03-28 21:47:34 +0530

    add fill frame

--- utils/read-module/src/lru.c
+++ utils/read-module/src/lru.c
@@ -3,13 +3,15 @@
 #include "algo.h"
 
 
-int is_in_set(algorithm* algo, int id) {
+int is_in_set(algorithm* algo) {
 	int i = 0;
+	// put lock
 	for(i = 0; i < algo->no_threads; ++i) {
-		if(algo->thread_set[i] == id) {
+		if(algo->thread_set[i] == algo->id) {
 			return 1;
 		}
 	}
+	//unlock
 	return 0;
 }
 
@@ -40,7 +42,8 @@ void update_frame_in_memory(algorithm* algo, int frame_no) {
 
 void fill_frame(algorithm* algo, struct page_stream_entry* stream_entry, long frame_no) {
 	table_entry_t* entry = kmalloc(sizeof(table_entry_t), GFP_ATOMIC);
-	table_entry_t* found;
+	table_entry_t* found = NULL;
+	table_entry_t* temp = NULL;
 
 	memset(entry, 0, sizeof(table_entry_t));
 	entry->key.pid = stream_entry->pid;
@@ -51,12 +54,18 @@ void fill_frame(algorithm* algo, struct page_stream_entry* stream_entry, long fr
 	HASH_FIND(hh, algo->page_tables, &(entry->key), sizeof(table_key_t), found);
 
 	if(found) {
-		// HASH_UPDATE
+		HASH_UPDATE(hh, algo->page_tables, key, sizeof(table_key_t), found, entry, temp);
+		kfree(entry);
 	}
 	else {
-		// HASH_ADD
+		HASH_ADD(hh, algo->page_tables, key, sizeof(table_key_t), entry);
 	}
 
+	// Update memory cell
+	update_frame_in_memory(algo, frame_no);
+	algo->memory[frame_no].pid = stream_entry->pid;
+	algo->memory[frame_no].virtual_page_no = stream_entry->virt_page_no;
+	algo->page_fault_count++;
 
 }
 
@@ -68,15 +77,13 @@ void call(void * arg) {
 	
 	while(*(algo->simulating)) {
 		// switching event wait
-		if(!is_in_set) {
+		if(!is_in_set(algo) {//TODO FIX THIS0
 			// Acquire Read Lock
 			// Event wait
 			entry = TAILQ_FIRST(algo->que);
 
-			if((pte = is_in_page_table(algo, entry))) {
-				if(pte->present_bit){
-					update_frame_in_memory(algo, pte->frame_no);
-				}
+			if((pte = is_in_page_table(algo, entry)) && (pte->present_bit)) {
+				update_frame_in_memory(algo, pte->frame_no);
 			}
 			else {
 				// free frame availabe
